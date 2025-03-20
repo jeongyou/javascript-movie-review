8d70780bff3135e291705ba490bfe6ed461d362f
youjeong Jeong <wjddbwjd1205@naver.com>
2025-03-20 15:56:13 +0900

    Updates

--- assets/index-BdV0ZUQq.js
+++ assets/index-BdV0ZUQq.js
@@ -205,7 +205,7 @@ class SearchBar {
       store.setMode("searchAdd");
     });
     const buttonImage = document.createElement("img");
-    buttonImage.src = "/images/find.png";
+    buttonImage.src = "images/find.png";
     buttonImage.alt = "검색";
     buttonImage.classList.add("search-icon");
     searchButton.appendChild(buttonImage);
